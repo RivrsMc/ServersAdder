96f0fcd5eb047ab7c9b28c709ee300767cf2997e
Roch Blonndiaux <contact@roch-blondiaux.com>
2025-01-24 03:12:47 +0100

    feat: maintenance service

--- velocity/src/main/java/io/rivrs/serversadder/ServersAdder.java
+++ velocity/src/main/java/io/rivrs/serversadder/ServersAdder.java
@@ -1,17 +1,22 @@
 package io.rivrs.serversadder;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 import org.slf4j.Logger;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 
+import co.aikar.commands.VelocityCommandManager;
+import io.rivrs.serversadder.commands.MaintenanceCommand;
+import io.rivrs.serversadder.maintenance.MaintenanceConfiguration;
 import io.rivrs.serversadder.redis.RedisManager;
 import io.rivrs.serversadder.server.ServerService;
 import lombok.Getter;
@@ -30,6 +35,8 @@ public class ServersAdder {
 
     private RedisManager redis;
     private ServerService service;
+    private MaintenanceConfiguration maintenance;
+    private VelocityCommandManager commands;
 
     @Inject
     public ServersAdder(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
@@ -47,6 +54,14 @@ public class ServersAdder {
         this.redis = new RedisManager(this);
         this.redis.load();
 
+        // Maintenance
+        this.maintenance = new MaintenanceConfiguration(this);
+        this.maintenance.load();
+
+        // Commands
+        this.commands = new VelocityCommandManager(this.server, this);
+        this.commands.registerCommand(new MaintenanceCommand());
+
         // Start health check
         this.service.startHealthCheck();
     }
@@ -56,7 +71,24 @@ public class ServersAdder {
         // Stop health check
         this.service.stopHealthCheck();
 
+        // Commands
+        this.commands.unregisterCommands();
+
+        // Maintenance
+        try {
+            this.maintenance.save();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save maintenance configuration", e);
+        }
+
         // Redis
         this.redis.close();
+    }
+
+    @Subscribe
+    public void onLogin(PreLoginEvent e) {
+        if (this.maintenance.isEnabled() && !this.maintenance.isAllowed(e.getUsername())) {
+            e.setResult(PreLoginEvent.PreLoginComponentResult.denied(this.maintenance.getKickMessage()));
+        }
     }
 }
